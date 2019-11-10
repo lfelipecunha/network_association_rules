4d11246b63b96a1a479a59e3537e3d7fad289c40
Luiz Felipe Cunha <felipe.silvacunha@gmail.com>
2019-11-10 12:19:09 -0300

    Added some comments to explain rules of synthetic generation; Fix base path of results and data

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@
 
 import util
 from nar import NetworkAssociationRules
+import os
 
 def parse_association_rule(row, index):
     row = association_rules[index]
@@ -17,22 +18,23 @@ def parse_accuracy(row, index):
     return [semester_hour[0], semester_hour[1], rule[0], rule[1], row,result]
 
 #util.generate_random_data('data.csv', 80000)
-trainning_data = util.get_data_from_file('../data_base/trainning_data.csv')
+base_path, filename = os.path.split(__file__)
+trainning_data = util.get_data_from_file(base_path + '/../data_base/trainning_data.csv')
 print("Generating association rules...")
 nar = NetworkAssociationRules(trainning_data)
 association_rules = nar.calculate_association_rules()
 
 # saving association_rules
 print("Saving association rules into file...")
-util.create_csv("../results/association_rules.csv", association_rules, ["Semester", "Hour", "level", "Conf", "Sup"], parse_association_rule)
+util.create_csv(base_path + "/../results/association_rules.csv", association_rules, ["Semester", "Hour", "level", "Conf", "Sup"], parse_association_rule)
 
 
 #util.generate_random_data('test_data.csv', 20000)
-test_data = util.get_data_from_file('../data_base/testing_data.csv')
+test_data = util.get_data_from_file(base_path + '/../data_base/testing_data.csv')
 print("Testing....")
 accuracy = nar.validate_association_rules(test_data)
 
 # saving accuracy
 print("Saving test results...")
-util.create_csv("../results/accuracy.csv", accuracy, ["Semester", "Hour", "level", "Conf[rule]", "Conf[test]", "Result"], parse_accuracy)
+util.create_csv(base_path + "/../results/accuracy.csv", accuracy, ["Semester", "Hour", "level", "Conf[rule]", "Conf[test]", "Result"], parse_accuracy)
 
--- src/sythetic_data_generator.py
+++ src/sythetic_data_generator.py
@@ -1,6 +1,7 @@
 from datetime import datetime, timedelta
 import random
 import csv
+import os
 
 
 def convert_datetime_to_semester_hour(date):
@@ -25,67 +26,71 @@ def get_factor_by_date(date):
     factor = 1.0
     month = date.month
 
-    if month < 3:
+    if month < 3: # initial months of year has only a few stutends
         factor = 0.2
     elif month == 3:
-        if date.day <= 10:
+        if date.day <= 10: # in first days of semester students not all students become
             factor = 0.75
-        else:
+        else: # after a few days it has the maximum of stundents
             factor = 1.0
-    elif month < 6:
+    elif month < 6: # the middle of semester students missing class
         factor = 0.8
-    elif month == 6:
+    elif month == 6: # the final of semester students start to back
         factor = 0.85
     elif month == 7:
-        if date.day <= 15:
+        if date.day <= 15: # the final days of semester the students has tests and increase the number of students
             factor = 0.9
-        else:
+        else: # the time between semesters decrease the amoung of students
             factor = 0.6
     elif month == 8:
-        if date.day <= 10:
-            factor = 0.65
+        if date.day <= 10: # the time between semesters decrease the amoung of students
+            factor = 0.6
         else:
-            factor = 0.95
-    elif month <= 11:
+            factor = 0.7 # the inital of second semester has no many students
+    elif month <= 11: # the middle of second semester has less students than first semester
         factor = 0.75
     else:
-        if date.day <= 15:
+        if date.day <= 15: # the final of second semester has tests so increase the number of students
             factor = 0.85
         else:
-            factor = 0.2
+            factor = 0.2 # vacations
     return factor
 
 
 def generate_data(filename, date, end_date):
     with open(filename, 'w') as csv_file:
         writer = csv.writer(csv_file, delimiter=";")
+
         while(date <= end_date):
             students_min = 0.02
             students_max = 0.1
             if is_class_day(date):
-                if date.weekday() < 5:
-                    if date.hour >= 7 and date.hour < 9:
+                if date.weekday() < 5: # monday to friday
+                    # in the mornings it has at least 25% of students
+                    if date.hour >= 7 and date.hour < 9: # few students become before class
                         students_min = 0.05
                         students_max = 0.2
-                    elif date.hour <= 11:
+                    elif date.hour <= 11: # maximum of mornings
                         students_min = 0.1
                         students_max = 0.25
-                    elif date.hour < 14:
+                    elif date.hour < 14: # lunch time, few students stay at university
                         students_min = 0.05
-                        students_max = 0.2
+                        students_max = 0.15
+                    # in the afternoons it has at least 25% of students
                     elif date.hour < 18:
                         students_min = 0.1
                         students_max = 0.25
-                    elif date.hour < 19:
+                    # in the night the most part of students become
+                    elif date.hour < 19: # few students become before night class
                         students_min = 0.3
                         students_max = 0.6
-                    elif date.hour < 23:
-                        students_min = 0.5
-                        students_max = 0.9
-                    else:
+                    elif date.hour < 23: # the maximum number of students
+                        students_min = 0.6
+                        students_max = 1.0
+                    else: # after class few students stay at university
                         students_min = 0.2
                         students_max = 0.5
-                elif date.weekday() == 5:
+                elif date.weekday() == 5: #saturday class is only mornings and has some extensions class
                     if date.hour >= 7 and date.hour < 9:
                         students_min = 0.1
                         students_max = 0.25
@@ -100,5 +105,6 @@ def generate_data(filename, date, end_date):
             date += timedelta(hours=1)
             writer.writerow([sh[0], sh[1], level])
 
-generate_data('../data_base/trainning_data.csv', datetime(2012, 1, 1),datetime(2017, 12, 31, 23))
-generate_data('../data_base/testing_data.csv', datetime(2011, 1, 1),datetime(2018, 12, 31, 23))
+base_path, filename = os.path.split(__file__)
+generate_data(base_path + '/../data_base/trainning_data.csv', datetime(2012, 1, 1),datetime(2017, 12, 31, 23))
+generate_data(base_path + '/../data_base/testing_data.csv', datetime(2011, 1, 1),datetime(2018, 12, 31, 23))
